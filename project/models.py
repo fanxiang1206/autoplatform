44b9a77754bc0dae4beb481c8a0e33e725c2adb5
zhaoyang <942790507@qq.com>
2019-07-09 19:45:56 +0800

    换电脑后第3次提交

--- project/models.py
+++ project/models.py
@@ -8,5 +8,6 @@ class Project(models.Model):
     status = models.BooleanField("状态",default=1)
     creattime = models.DateTimeField("创建时间",auto_now_add=True)
 
+
     def __str__(self):
         return self.name
