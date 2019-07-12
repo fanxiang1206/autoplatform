b3c552a9cef6942d7f8a0d84cc5101a8f089d75f
zhaoyang <942790507@qq.com>
2019-07-12 12:02:25 +0800

    js静态文件配置

---
+++ static/js/common.js
@@ -0,0 +1,66 @@
+    function debug() {
+        var reg_url = $("#reg_url").val();
+        var req_method = $('input:radio[name="req_method"]:checked').val();
+        var reg_header =$("#reg_header").val();
+        var req_type =$('input:radio[name="req_type"]:checked').val();
+        var reg_param =$("#reg_param").val();
+
+        if(reg_url == ""){
+            alert("请求的url不能为空！")
+            $("#reg_url").focus()
+            return;
+        }
+
+        $.ajax({
+            type:"POST",
+            url:'/case/debug/',
+            data:{
+                reg_url:reg_url,
+                req_method:req_method,
+                reg_header:reg_header,
+                req_type:req_type,
+                reg_param:reg_param
+            },
+            success:function (result) {
+
+                $("#req_result").val(result);
+
+            }
+        });
+
+    };
+
+
+    function req_assert() {
+        var req_assert_type = $('input:radio[name="req_assert_type"]:checked').val();
+        var req_assert_param =$("#req_assert_param").val();
+        var req_result =$("#req_result").val();
+
+        if(req_assert_param == ""){
+            alert("断言的预期结果不能为空！")
+            $("#req_assert_param").focus()
+            return;
+        }
+
+        if(req_result == ""){
+            alert("断言的实际结果不能为空！")
+            $("#req_result").focus()
+            return;
+        }
+
+        $.ajax({
+            type:"POST",
+            url:'/case/req_assert/',
+            data:{
+                req_assert_type:req_assert_type,
+                req_assert_param:req_assert_param,
+                req_result:req_result,
+            },
+            success:function (result) {
+
+                alert(result)
+
+            }
+        });
+
+    };
