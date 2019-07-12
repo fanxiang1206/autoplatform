31b5b7e08391c5bd25d8ec6d62acbdab70b1b4ea
zhaoyang <942790507@qq.com>
2019-07-12 09:58:01 +0800

    case的debug调试完毕

--- case/views.py
+++ case/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from case.httpclient import httpclient
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 import json
 
 # Create your views here.
@@ -24,37 +24,30 @@ def add(request):
 def debug(request):
 
     reg_url = request.POST.get("reg_url","")
-    req_method = request.POST.get("req_method"),
-    reg_header = request.POST.get("reg_header",""),
-    req_type = request.POST.get("req_type",""),
+    req_method = request.POST.get("req_method")
+    reg_header = request.POST.get("reg_header","")
+    req_type = request.POST.get("req_type","")
     reg_param = request.POST.get("reg_param","")
 
-    client = httpclient(url=reg_url,method=req_method[0],body_type=req_type[0])
+    #初始化httpclient对象
+    client = httpclient(url=reg_url,method=req_method,body_type=req_type)
 
     if reg_url == '' or reg_url is None:
 
-        return JsonResponse({
-            "errocde": 1001,
-            "errdesc": "失败,请求地址不能为空！！！",
-            "result": ""
-        })
-    #
-    # print("3333")
-    # print(reg_header)
-    # print(reg_header[0])
-    #
-    # print("4444")
-    client.set_headers(json.loads(reg_header[0]))
+        return HttpResponse("请求的url不能为空！！！")
 
-    # if reg_param[0] != '' and reg_param[0] is not None:
-    #     client.set_params(reg_param)
+    if reg_header != '' and reg_header is not None:
+
+        client.set_headers(json.loads(reg_header))
+
+    if reg_param != '' and reg_param is not None:
+
+        client.set_params(json.loads(reg_param))
 
     client.send()
 
-    print(type(client.res.json()))
+    # print(client.res.json())
 
-    return JsonResponse({
-        "errcode":0,
-        "errdesc":"成功",
-        "data":client.res.json()
-    })
+    # {"x-qp-appid": "21001", "x-qp-gid": "1", "x-qp-userid": "1"}
+
+    return HttpResponse(str(client.res.json()))
